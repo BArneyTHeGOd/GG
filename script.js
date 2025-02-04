3bb40529ae9348e16f6c2e67abb2e5af5bed21ce
BArneyTHeGOd <arnaav05@gmail.com>
2025-02-04 09:16:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function checkPassword() {
-  const password = document.getElementById('passwordInput').value;
-  const correctPassword = 'dayone'; // Change this to your desired password
+    const password = document.getElementById('passwordInput').value;
+    const correctPassword = 'dayone';
+    const message = document.getElementById('message');
 
-  if (password === correctPassword) {
-    document.getElementById('passwordContainer').style.display = 'none';
+    if (password === correctPassword) {
+        document.getElementById('passwordContainer').style.display = 'none';
+        document.getElementById('editorContainer').style.display = 'block';
+    } else {
+        message.textContent = "Incorrect password. Please try again.";
+        message.style.color = "red";
+    }
+}
+
+function redirectToWebsite(url) {
+    window.open(url, '_blank');
+}
