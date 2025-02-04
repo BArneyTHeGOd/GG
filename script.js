28ec84549fee2fcff7885920672ed6339f04d451
BArneyTHeGOd <arnaav05@gmail.com>
2025-02-04 09:11:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 function checkPassword() {
   const password = document.getElementById('passwordInput').value;
   const correctPassword = 'dayone'; // Change this to your desired password
+
   if (password === correctPassword) {
     document.getElementById('passwordContainer').style.display = 'none';
-    document.getElementById('editorContainer').style.display = 'flex';
-  } else {
-    document.getElementById('message').innerText = 'Incorrect password. Access denied.';
-  }
-}
-function redirectToWebsite(url) {
-  window.location.href = url;
-}
-    const password = document.getElementById('passwordInput').value;
-    const correctPassword = 'dayone';
-
-    if (password === correctPassword) {
-        document.getElementById('passwordContainer').style.display = 'none';
-        document.getElementById('editor
