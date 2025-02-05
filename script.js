4fa8c694d8988e197f2567672c42f55801325791
BArneyTHeGOd <arnaav05@gmail.com>
2025-02-04 17:21:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function checkPassword() {
-    const password = document.getElementById('passwordInput').value;
-    const correctPassword = 'dayone';
-    const message = document.getElementById('message');
-
-    if (password === correctPassword) {
-        document.getElementById('passwordContainer').style.display = 'none';
-        document.getElementById('editorContainer').style.display = 'block';
-    } else {
-        message.textContent = "Incorrect password. Please try again.";
-        message.style.color = "red";
-    }
+  const password = document.getElementById('passwordInput').value;
+  const correctPassword = 'dayone';
+  const message = document.getElementById('message');
+  if (password === correctPassword) {
+    document.getElementById('passwordContainer').style.display = 'none';
+    document.getElementById('editorContainer').style.display = 'block';
+  } else {
+    message.textContent = "Incorrect password. Please try again.";
+    message.style.color = "red";
+  }
 }
 
 function redirectToWebsite(url) {
-    window.open(url, '_blank');
+  window.open(url, '_blank');
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
 }
